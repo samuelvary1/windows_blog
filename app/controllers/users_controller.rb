95115e5c097ddf80babbca82c934a02b39767839
Sam Vary <sam.vary@gmail.com>
2014-12-05 18:04:17 -0500

    edited users controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
 
   def edit
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:notice] = "Registration successful."
+  		redirect_to root_url
+  	else
+  		render 'new'
   end
 end
